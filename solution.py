26fda06a6c44914f122dffd02b9ccb4039ffa9eb
semenkulemin <55997982+semenkulemin@users.noreply.github.com>
2023-04-13 16:16:54 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-import scipy.stats as sps
+from scipy.stats import mannwhitneyu
+# import scipy.stats as sps
 
 
 chat_id = 341395919 # Ваш chat ID, не меняйте название переменной
@@ -9,9 +10,11 @@ def solution(x: np.array, y: np.array) -> bool: # Одна или две выб�
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    p_value = sps.permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis), 
-             vectorized=True, 
-             n_resamples=500,
-             alternative='greater').pvalue 
-    alpha = 0.05
-    return p_value < alpha # Ваш ответ, True или False
+#     p_value = sps.permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis), 
+#              vectorized=True, 
+#              n_resamples=500,
+#              alternative='greater').pvalue 
+#     alpha = 0.05
+#     return p_value < alpha # Ваш ответ, True или False
+    pval = mannwhitneyu(x, y, alternative="less").pvalue
+    return pval < 0.05
