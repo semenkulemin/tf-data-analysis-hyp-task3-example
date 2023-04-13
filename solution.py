75131013a476968abfb81ac34fdf296469d52664
semenkulemin <55997982+semenkulemin@users.noreply.github.com>
2023-04-13 14:51:18 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as sps
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 341395919 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x: np.array, y: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    p_value = sps.permutation_test((x, y), lambda x, y, axis: np.mean(x, axis=axis) - np.mean(y, axis=axis), 
+             vectorized=True, 
+             n_resamples=500,
+             alternative='greater').pvalue 
+    alpha = 0.05
+    return p_value < alpha # Ваш ответ, True или False
